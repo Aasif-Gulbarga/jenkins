51c0da1896e71701439ff2040bc7f7e62dee229f
AasifTolasabGulbarga <aasif.gulbarga@williamslea.com>
2026-06-13 01:59:51 +0530

    first commit

---
+++ src/main/java/jenkins_demo/jenkins/JenkinsController.java
@@ -0,0 +1,13 @@
+package jenkins_demo.jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsController {
+
+    @GetMapping("/jenkis")
+    public String index() {
+        return "Hello Jenkins!";
+    }
+}
